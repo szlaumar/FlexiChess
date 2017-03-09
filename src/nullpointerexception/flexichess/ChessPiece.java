5e7c86e11fc22f8d30861bf1188e3153a20dbc11
drabekj <drabek.honza@gmail.com>
2017-03-09 18:47:56 +0100

    Add TEST 3 homework code.

---
+++ src/nullpointerexception/flexichess/ChessPiece.java
@@ -0,0 +1,142 @@
+package nullpointerexception.flexichess;
+
+/**
+ * Představuje šachovou figurku.
+ * 
+ * @author Drabek Jan
+ * @author Stastny Martin
+ * @author Szlauer Martin
+ */
+public class ChessPiece {
+    
+    /**
+     * Defines ChessPiece/chess figure color and assigned sign for printing.
+     */
+    public enum Color { 
+        WHITE('+'), 
+        BLACK('-');
+        
+        /**
+         * Sign assigned to color for short printing.
+         */
+        public final char m_sign;
+
+        private Color(char sign){
+            m_sign = sign;
+        }
+
+        public char getSign() {
+            return m_sign;
+        }
+    }
+
+    /** 
+     * Letter describes ChessPiece/chess figure kind by standard chess notation.
+     * Different kinds has different behaviors.
+     * <p>
+     * 
+     * CHESS FIGURE KINDS:  <br>
+     *  P   pawn            <br>
+     *  N   knight          <br>
+     *  B   bishop          <br>
+     *  R   rook            <br>
+     *  Q   queen           <br>
+     */
+    private final char  m_letter;
+    private final Color m_color;
+    private Square      m_square = null;
+    
+    /**
+     * Vytvoří figurku označenou písmenem letter s barvou color.
+     * 
+     * @param letter    Letter which describes chess figure by chess notation.    
+     * @param color     Color of figure. BLACK or WHITE.
+     */
+    public ChessPiece(char letter, Color color) {
+        m_letter = letter;
+        m_color = color;
+    }
+
+    /**
+     * Vrátí barvu figurky.
+     * 
+     * @return ChessPiece/chess figure color.
+     */
+    public ChessPiece.Color color(){
+        return m_color;
+    }
+    
+    /**
+     * Vrátí písmenko figurky.
+     * 
+     * @return ChessPiece/chess figure letter.
+     */
+    public char letter(){
+        return m_letter;
+    }
+    
+    /**
+     * Vrátí symbol pro figurku, například +A, -B a podobně, viz zadání úkolu.
+     * 
+     * @return ChessPiece/chess figure symbol.
+     */
+    public String symbol(){
+        return "" + m_color.getSign() + m_letter;
+    }
+
+    /**
+     * Vrátí čtverec, na kterém zrovna figurka stojí. 
+     * 
+     * Pokud je figurka mimo šachovnici, vyhodí IllegalStateException.
+     * 
+     * @return Square/board location.
+     */
+    public Square square(){
+        if( this.isOffBoard() )
+            throw new IllegalStateException("ChessPiece is off board!");
+        else
+            return m_square;
+    }
+
+    /**
+     * Nastaví figurce čtverec, na kterém má stát. 
+     * 
+     * Pokud je paramter null vyhodí výjimku NullPointerException
+     * 
+     * @param square Square where to put ChessPiece/chess figure.
+     */
+    public void setSquare(Square square ){
+        if(square == null)
+            throw new NullPointerException("Can't assign null position on board.");
+        m_square = square;
+    }
+
+    /**
+     * Po zavolání této metody nemá figurka žádný čtverec, na kterém stojí. 
+     * 
+     * Je tedy mimo šachovnici.
+     */
+    public void setOffBoard(){
+        m_square = null;
+    }
+    
+    /**
+     * Vrátí True pokud nemá figurka přiřazen žádný čtverec na šachovnici.
+     * 
+     * @return True if ChessPiece/chess figure hasn't assigned Square/board location.
+     */
+    public boolean isOffBoard(){
+        return null == m_square;
+    }
+    
+    /**
+     * Vrátí totéž, co metoda symbol().
+     * 
+     * @return Return output from symbol() method.
+     */
+    @Override
+    public String toString() {
+        return symbol();
+    }
+    
+}
---
+++ src/nullpointerexception/flexichess/Square.java
@@ -0,0 +1,101 @@
+package nullpointerexception.flexichess;
+
+/**
+ * Představuje jeden čtverec šachovnice.
+ * 
+ * @author Drabek Jan
+ * @author Stastny Martin
+ * @author Szlauer Martin
+ */
+public class Square {
+    
+    private final char  m_column;
+    private final int   m_row;
+    private ChessPiece  m_piece = null;
+    
+    /**
+     * Vytvoří čtverec s danými souřadnicemi.
+     * 
+     * @param column    Column letter.
+     * @param row       Row number.
+     */
+    public Square(char column, int row){
+        m_column = column;
+        m_row    = row;
+    }
+    
+    /**
+     * Vrátí písmeno sloupce daného čtverce.
+     * 
+     * @return  Column letter.
+     */
+    public char column(){
+        return m_column;
+    }
+    
+    /**
+     * Vrátí číslo řádku daného čtverce.
+     * 
+     * @return  Row number.
+     */
+    public int row(){
+        return m_row;
+    }
+    
+    /**
+     * Vrátí figurku na tomto čtverci. 
+     * 
+     * Pokud je čtverec prázný, vyhodí výjimku IllegalStateException.
+     * 
+     * @return  ChessPiece/chess figure.
+     */
+    public ChessPiece piece(){
+        if( this.isEmpty() )
+            throw new IllegalStateException("Square is empty!");
+        else
+            return m_piece;
+    }
+    
+    /**
+     * Postaví figurku na tento čtverec. 
+     * 
+     * Figurce se tak zároveň nastaví tento čtverec jak ten, na kterém stojí. 
+     * Pokud už na čtverci nějaká figurka stojí, vyhodí výjimku IllegalStateException.
+     * 
+     * @param piece  ChessPiece/chess figure.
+     */
+    public void putPiece(ChessPiece piece){
+        if( m_piece != null)
+            throw new IllegalStateException("Square isn't empty!");
+        m_piece = piece;
+        m_piece.setSquare(this);
+    }
+    
+    /**
+     * Pravda, je-li čtverec prázdný (bez figurky).
+     * 
+     * @return True if square hasn't assigned ChessPiece/chess figure.
+     */
+    public boolean isEmpty(){
+        return null == m_piece;
+    }
+    
+    /**
+     * Odstraní ze čtverce figurku a vrátí ji jako výsledek volání, 
+     * pokud na něm nějaká stojí. 
+     * 
+     * Figurka se tím ocitne mimo šachovnici. 
+     * Pokud je čtverec prázný, vyhodí výjimku IllegalStateException.
+     * 
+     * @return 
+     */
+    public ChessPiece empty(){
+        ChessPiece tmp_piece = piece();
+        tmp_piece.setOffBoard();
+
+        m_piece = null;
+
+        return tmp_piece;
+    }
+    
+}
