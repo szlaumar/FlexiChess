087274e5668291cc005468db111c46a49090b34c
Drabek Jan <j.drabekj@gmail.com>
2017-03-18 23:14:36 +0000

    + Implemented Demichess builder.

--- src/nullpointerexception/flexichess/FlexiChess.java
+++ src/nullpointerexception/flexichess/FlexiChess.java
@@ -1,5 +1,7 @@
 package nullpointerexception.flexichess;
 
+import nullpointerexception.flexichess.builders.Demichess;
+
 /**
  * 
  * @author Drabek Jan
@@ -10,63 +12,12 @@ public class FlexiChess {
 
     public static void main(String[] args) {
         
-        System.out.println("FlexiChess v3.0\n");
+        System.out.println("FlexiChess v4.0\n");
 
-        ChessBoard board = new ChessBoard(4, 4);
-        ChessBoardAsString boardAsString = new ChessBoardAsString(board);
+        Demichess demichess = new Demichess();
+        ChessBoard board = demichess.board();
 
-        System.out.println("Printing test #3:\n");
-//        board.putPiece('a', 1, new ChessPiece('R', ChessPiece.Color.WHITE));
-        board.putPiece('a', 1, new Rook(board, ChessPiece.Color.WHITE));
-//        board.putPiece('b', 1, new ChessPiece('Q', ChessPiece.Color.WHITE));
-        board.putPiece('b', 1, new Queen(board, ChessPiece.Color.WHITE));
-//        board.putPiece('c', 1, new ChessPiece('K', ChessPiece.Color.WHITE));
-        board.putPiece('c', 1, new King(board, ChessPiece.Color.WHITE));
-//        board.putPiece('d', 1, new ChessPiece('R', ChessPiece.Color.WHITE));
-        board.putPiece('d', 1, new Rook(board, ChessPiece.Color.WHITE));
-
-//        board.putPiece('a', 2, new ChessPiece('p', ChessPiece.Color.WHITE));
-        board.putPiece('a', 2, new Pawn(board, ChessPiece.Color.WHITE));
-//        board.putPiece('b', 2, new ChessPiece('p', ChessPiece.Color.WHITE));
-        board.putPiece('b', 2, new Pawn(board, ChessPiece.Color.WHITE));
-//        board.putPiece('c', 2, new ChessPiece('p', ChessPiece.Color.WHITE));
-        board.putPiece('c', 2, new Pawn(board, ChessPiece.Color.WHITE));
-//        board.putPiece('d', 2, new ChessPiece('p', ChessPiece.Color.WHITE));
-        board.putPiece('d', 2, new Pawn(board, ChessPiece.Color.WHITE));
-//
-//        board.putPiece('a', 3, new ChessPiece('p', ChessPiece.Color.BLACK));
-        board.putPiece('a', 3, new Pawn(board, ChessPiece.Color.BLACK));
-//        board.putPiece('b', 3, new ChessPiece('p', ChessPiece.Color.BLACK));
-        board.putPiece('b', 3, new Pawn(board, ChessPiece.Color.BLACK));
-//        board.putPiece('c', 3, new ChessPiece('p', ChessPiece.Color.BLACK));
-        board.putPiece('c', 3, new Pawn(board, ChessPiece.Color.BLACK));
-//        board.putPiece('d', 3, new ChessPiece('p', ChessPiece.Color.BLACK));
-        board.putPiece('d', 3, new Pawn(board, ChessPiece.Color.BLACK));
-//
-//        board.putPiece('a', 4, new ChessPiece('R', ChessPiece.Color.BLACK));
-        board.putPiece('a', 4, new Rook(board, ChessPiece.Color.BLACK));
-//        board.putPiece('b', 4, new ChessPiece('Q', ChessPiece.Color.BLACK));
-        board.putPiece('b', 4, new Queen(board, ChessPiece.Color.BLACK));
-//        board.putPiece('c', 4, new ChessPiece('K', ChessPiece.Color.BLACK));
-        board.putPiece('c', 4, new King(board, ChessPiece.Color.BLACK));
-//        board.putPiece('d', 4, new ChessPiece('R', ChessPiece.Color.BLACK));
-        board.putPiece('d', 4, new Rook(board, ChessPiece.Color.BLACK));
-
-        String expected = 
-                        "   ┌───┬───┬───┬───┐\n" + 
-                        " 4 │-R │-Q │-K │-R │\n" +
-                        "   ├───┼───┼───┼───┤\n" +
-                        " 3 │-p │-p │-p │-p │\n" +
-                        "   ├───┼───┼───┼───┤\n" +
-                        " 2 │+p │+p │+p │+p │\n" +
-                        "   ├───┼───┼───┼───┤\n" +
-                        " 1 │+R │+Q │+K │+R │\n" +
-                        "   └───┴───┴───┴───┘\n" +
-                        "     a   b   c   d "; 
-
-        System.out.print(expected + "\n");
         System.out.print(board.toString() + "\n");
-        System.out.print(boardAsString.toString() + "\n");        
     }
     
 }
