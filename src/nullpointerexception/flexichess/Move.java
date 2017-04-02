0ffe4c4d9e7c73f58633e9ec05d9313de7ec31f7
drabekj <drabek.honza@gmail.com>
2017-04-02 17:29:54 +0200

    + Martin Stastny, task 6.

---
+++ src/nullpointerexception/flexichess/Move.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nullpointerexception.flexichess;
+
+/**
+ * @author Drabek Jan
+ * @author Stastny Martin
+ * @author Szlauer Martin
+ */
+public interface Move {
+    public void executeOnBoard(ChessBoard board);
+    public void revertOnBoard(ChessBoard board);
+    public String notation();
+}
